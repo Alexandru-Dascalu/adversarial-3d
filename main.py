04ba5aae7232a9e46c8b142249fac14af2c5ca83
Alexandru Dascalu <alexandru.dascalu100@gmail.com>
2022-08-04 16:24:56 +0100

    now plotting loss and tfr history

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
 import numpy as np
+import matplotlib.pyplot as plt
 import tensorflow as tf
+
 gpu = tf.config.list_physical_devices('GPU')[0]
 tf.config.experimental.set_memory_growth(gpu, True)
 tf.config.set_logical_device_configuration(
-        gpu,
-        [tf.config.LogicalDeviceConfiguration(memory_limit=3800)])
+    gpu,
+    [tf.config.LogicalDeviceConfiguration(memory_limit=3800)])
 
 from PIL import Image
 from renderer import Renderer
 from net import AdversarialNet
 from config import cfg
 
-LOGGING_ENABLED = False
+FILE_LOGGING_ENABLED = False
 
 
 def main():
@@ -32,6 +34,8 @@ def main():
     with tf.device("/GPU:0"):
         # create the adversarial texture model that will be optimised. Holds all relevant tensors.
         model = AdversarialNet(texture)
+        loss_history = []
+        tfr_history = []
         for i in range(cfg.iterations):
             # UV mapping is a numpy array of shape batch_size x texture_width x texture_height x 2
             uv = renderer.render(cfg.batch_size)
@@ -41,13 +45,12 @@ def main():
 
             # optimise adversarial texture
             model.optimisation_step(uv)
-            print("Epoch: {}".format(i))
-            print('Loss: {}'.format(model.loss))
-            print('Diff: {}'.format(model.get_diff().sum()))
-            print('Prediction:\n{}'.format(model.top_k_predictions))
+            loss_history.append(model.loss)
+            tfr_history.append(get_tfr(model.top_k_predictions))
 
-            if LOGGING_ENABLED:
-                log_trainning(model, log_writer, i)
+            log_training_to_console(model, i)
+            if FILE_LOGGING_ENABLED:
+                log_training_to_file(model, log_writer, i)
 
             # save intermediate adversarial textures
             if i % 10 == 0:
@@ -55,10 +58,19 @@ def main():
                 adv_texture = Image.fromarray(adv_texture.astype(np.uint8))
                 adv_texture.save('{}/adv_{}.jpg'.format(cfg.image_dir, i))
 
+        plot_training_history(loss_history, tfr_history)
+
     log_writer.close()
 
 
-def log_trainning(model, writer, epoch):
+def log_training_to_console(model, step):
+    print("Step: {}".format(step))
+    print('Loss: {}'.format(model.loss))
+    print('Diff: {}'.format(model.get_diff().sum()))
+    print('Prediction:\n{}'.format(model.top_k_predictions))
+
+
+def log_training_to_file(model, writer, step):
     """
     Logs details of training steps to TF summary file writer.
 
@@ -68,15 +80,41 @@ def log_trainning(model, writer, epoch):
         The Adversarial texture model currently being trained.
     writer : tf.summary.SummaryWriter
         The writer used for logging.
-    epoch : int
+    step : int
         The training step currently being logged.
     """
     with writer.as_default():
-        tf.summary.image('train/std_images', model.std_images, step=epoch)
-        tf.summary.image('train/adv_images', model.adv_images, step=epoch)
-        tf.summary.scalar('train/loss', model.loss, step=epoch)
-        tf.summary.histogram('train/top_k_predictions', model.top_k_predictions, step=epoch)
+        tf.summary.image('train/std_images', model.std_images, step=step)
+        tf.summary.image('train/adv_images', model.adv_images, step=step)
+        tf.summary.scalar('train/loss', model.loss, step=step)
+        tf.summary.histogram('train/top_k_predictions', model.top_k_predictions, step=step)
         writer.flush()
+
+
+def get_tfr(top_k_predictions):
+    assert top_k_predictions.shape == (cfg.batch_size, 5)
+
+    target_reached = 0
+    for top_prediction in top_k_predictions[:, 0]:
+        if top_prediction == cfg.target:
+            target_reached += 1
+
+    tfr = target_reached / cfg.batch_size
+    return tfr
+
+
+def plot_training_history(loss_history, tfr_history):
+    plt.plot(loss_history, label="Loss")
+    plt.xlabel("Steps")
+    plt.ylabel("Loss")
+    plt.title("Loss during training")
+    plt.show()
+
+    plt.plot(tfr_history, label="TFR")
+    plt.xlabel("Steps")
+    plt.ylabel("Accuracy")
+    plt.title("TFR during training")
+    plt.show()
 
 
 if __name__ == '__main__':
